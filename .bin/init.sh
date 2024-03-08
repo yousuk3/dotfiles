9a430c4e6f80d47330747b4ec6e17e9e3a5c6b1c
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2024-03-08 15:31:37 +0900

    init.sh fix

--- .bin/init.sh
+++ .bin/init.sh
@@ -16,6 +16,11 @@ xcode-select --install
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-if [ "$(uname -m)" = "arm64" ] ; then
+
+# Add brew to your PATH
+if [ "$(uname -m)" = "x86_64" ] ; then
+  (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
+  eval "$(/usr/local/bin/brew shellenv)" > /dev/null
+elif [ "$(uname -m)" = "arm64" ] ; then
   eval "$(/opt/homebrew/bin/brew shellenv)" > /dev/null
 fi
