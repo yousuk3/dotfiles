539466f2a554417592b4bcaf3930cb3f9f457032
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2026-05-21 09:16:02 +0900

    fix

--- .bin/defaults.sh
+++ .bin/defaults.sh
@@ -26,7 +26,7 @@ fi
 ################################################################################
 
 ## ファイアウォール > ファイアウォール
-#sudo defaults write /Library/Preferences/com.Apple.alf globalstate -int 1
+sudo defaults write /Library/Preferences/com.Apple.alf globalstate -int 1
 
 
 ################################################################################
@@ -34,10 +34,10 @@ fi
 ################################################################################
 
 ## サウンドエフェクト > 起動時にサウンドを再生　オフ
-#sudo nvram StartupMute=%01
+sudo nvram StartupMute=%01
 
 ## サウンドエフェクト > 音量を変更するときにフィードバックを再生 オン
-#defaults write -g com.apple.sound.beep.feedback -bool "true"
+defaults write -g com.apple.sound.beep.feedback -bool "true"
 
 
 ################################################################################
@@ -45,13 +45,13 @@ fi
 ################################################################################
 
 ## サイドバーのアイコンサイズ　小
-#defaults write -g NSTableViewDefaultSizeMode -int "1"
+defaults write -g NSTableViewDefaultSizeMode -int "1"
 
 ## スクロールバーを表示 > 常に表示
-#defaults write -g AppleShowScrollBars -string "Always"
+defaults write -g AppleShowScrollBars -string "Always"
 
 ## スクロールバーをクリックして > クリックされた部分へジャンプ
-#defaults write -g AppleScrollerPagingBehavior -bool "true"
+defaults write -g AppleScrollerPagingBehavior -bool "true"
 
 
 ################################################################################
@@ -59,9 +59,9 @@ fi
 ################################################################################
 
 ## ポインタコントロール > トラックパッドオプション > ドラッグにトラックパッドを使用　オン　ドラッグ方法: 3本指のドラッグ
-#defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool "true"
-#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool "true"
-#defaults -currentHost write -g com.apple.trackpad.threeFingerDragGesture -bool "true"
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool "true"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool "true"
+defaults -currentHost write -g com.apple.trackpad.threeFingerDragGesture -bool "true"
 
 
 ################################################################################
@@ -69,19 +69,19 @@ fi
 ################################################################################
 
 ## コントロールセンターモジュール > Bluetooth > メニューバーに表示
-#defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool "true"
+defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool "true"
 
 ## コントロールセンターモジュール > サウンド > メニューバーに常に表示
-#defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool "true"
+defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool "true"
 
 ## その他のモジュール > バッテリー > 割合（％）を表示
-#defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool "true"
+defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool "true"
 
 ## メニューバーのみ > Spotlight > メニューバーに非表示
-#defaults -currentHost write com.apple.Spotlight MenuItemHidden -int 1
+defaults -currentHost write com.apple.Spotlight MenuItemHidden -int 1
 
 ## メニューバーのみ > Siri > メニューバーに非表示　元々非表示
-#defaults write com.apple.Siri StatusMenuVisible -bool "false"
+defaults write com.apple.Siri StatusMenuVisible -bool "false"
 
 
 ################################################################################
@@ -92,34 +92,34 @@ fi
 #defaults write com.apple.dock tilesize -int 36
 
 ## Dock > ウインドウをしまうときのエフェクト: スケールエフェクト
-#defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock mineffect -string "scale"
 
 ## Dock > ウインドウをアプリケーションアイコンにしまう　オン
-#defaults write com.apple.dock minimize-to-application -bool "true"
+defaults write com.apple.dock minimize-to-application -bool "true"
 
 ## Dock > Dockを自動的に表示/非表示　オン
-#defaults write com.apple.dock autohide -bool "true"
+defaults write com.apple.dock autohide -bool "true"
 
 ## アプリの提案と最近使用したアプリをDockに表示　オフ
-#defaults write com.apple.dock show-recents -bool "false"
+defaults write com.apple.dock show-recents -bool "false"
 
 ## デスクトップとステージマネージャ > 壁紙をクリックしてデスクトップを表示 > ステージマネージャ使用時のみ
-#defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool "false"
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool "false"
 
 ## Mission Control > 最新の使用状況に基づいて操作スペースを自動的に並び替える　オフ
-#defaults write com.apple.dock mru-spaces -bool "false"
+defaults write com.apple.dock mru-spaces -bool "false"
 
 ## ホットコーナー > 左下 > デスクトップ
-#defaults write com.apple.dock wvous-bl-corner -int 4
-#defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-modifier -int 0
 
 ## ホットコーナー > 右上 > ディスプレイをスリープさせる
-#defaults write com.apple.dock wvous-tr-corner -int 10
-#defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 10
+defaults write com.apple.dock wvous-tr-modifier -int 0
 
 ## ホットコーナー > 右下 > クイックメモ
-#defaults write com.apple.dock wvous-br-corner -int 14
-#defaults write com.apple.dock wvous-br-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 14
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 
 ################################################################################
@@ -127,7 +127,7 @@ fi
 ################################################################################
 
 ## スクリーンセーバを開始 > しない
-#defaults -currentHost write com.apple.screensaver idleTime -int 0
+defaults -currentHost write com.apple.screensaver idleTime -int 0
 
 
 ################################################################################
@@ -135,25 +135,25 @@ fi
 ################################################################################
 
 ## キーのリピート速度（最速）元は最速から2番目
-#defaults write -g KeyRepeat -int 2
+defaults write -g KeyRepeat -int 2
 
 ## リピート入力認識までの時間（最速）元は最速から3番目
-#defaults write -g InitialKeyRepeat -int 15
+defaults write -g InitialKeyRepeat -int 15
 
 # キーボードナビゲーション
-#defaults write -g AppleKeyboardUIMode -int 2
+defaults write -g AppleKeyboardUIMode -int 2
 
 ## キーボードショートカット > ファンクションキー > F1、　F2などのキーを標準のファンクションキーとして使用
-#defaults write -g com.apple.keyboard.fnState -bool "true"
+defaults write -g com.apple.keyboard.fnState -bool "true"
 
 ## テキスト入力 > 入力ソース > 編集 > すべての入力ソース > 英字入力中にスペルを自動変換
-#defaults write -g NSAutomaticSpellingCorrectionEnabled -bool "false"
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool "false"
 
 ## テキスト入力 > 入力ソース > 編集 > すべての入力ソース > 文頭を自動的に大文字にする
-#defaults write -g NSAutomaticCapitalizationEnabled -bool "false"
+defaults write -g NSAutomaticCapitalizationEnabled -bool "false"
 
 ## テキスト入力 > 入力ソース > 編集 > すべての入力ソース > スペースバーを2回押してピリオドを入力
-#defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool "false"
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool "false"
 
 
 ################################################################################
@@ -161,16 +161,16 @@ fi
 ################################################################################
 
 ## 軌跡の速さ（最速から４番目）元は最速から7番目
-#defaults write -g com.apple.mouse.scaling 1.5
+defaults write -g com.apple.mouse.scaling 1.5
 
 ## ナチュラルなスクロール　オフ
-#defaults write -g com.apple.swipescrolldirection -bool "false"
+defaults write -g com.apple.swipescrolldirection -bool "false"
 
 ## スクロールの速さ（最速から２番め）元は最速から5番目
-#defaults write -g com.apple.scrollwheel.scaling 4
+defaults write -g com.apple.scrollwheel.scaling 4
 
 ## 詳細設定 > ポインタを加速
-#defaults write -g com.apple.mouse.linear -bool "true"
+defaults write -g com.apple.mouse.linear -bool "true"
 
 
 ################################################################################
@@ -178,12 +178,12 @@ fi
 ################################################################################
 
 ## 軌跡の速さ（最速）元は最速から7番目
-#defaults write -g com.apple.trackpad.scaling 3
+defaults write -g com.apple.trackpad.scaling 3
 
 ## タップでクリック（すでにONだった）
-#defaults write com.apple.AppleMultitouchTrackpad Clicking -bool "true"
-#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool "true"
-#defaults -currentHost write -g com.apple.mouse.tapBehavior -bool "true"
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool "true"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool "true"
+defaults -currentHost write -g com.apple.mouse.tapBehavior -bool "true"
 
 
 ################################################################################
@@ -191,29 +191,29 @@ fi
 ################################################################################
 
 ## Finder > 設定 > 一般 > 新規Finderウインドウで次を表示:
-#defaults write com.apple.finder NewWindowTarget -string "PfHm"
-#defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
 ## Finder > 設定 > 詳細 > すべてのファイル名拡張子を表示
-#defaults write -g AppleShowAllExtensions -bool "true"
+defaults write -g AppleShowAllExtensions -bool "true"
 
 ## Finder > 設定 > 詳細 > 拡張子を変更する前に警告を表示
-#defaults write com.apple.finder FXEnableExtensionChangeWarning -bool "false"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool "false"
 
 ## Finder > 設定 > 詳細 > iCloud Driveから削除する前に警告を表示
-#defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool "false"
+defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool "false"
 
 ## Finder > 設定 > 詳細 > 検索実行時: > 現在のフォルダ内を検索
-#defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 ## Finder > 表示 > リスト
-#defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 ## Finder > 表示 > パスバーを表示
-#defaults write com.apple.finder ShowPathbar -bool "true"
+defaults write com.apple.finder ShowPathbar -bool "true"
 
 ## Finder > 表示 > ステータスバーを表示
-#defaults write com.apple.finder ShowStatusBar -bool "true"
+defaults write com.apple.finder ShowStatusBar -bool "true"
 
 
 ################################################################################
@@ -221,10 +221,10 @@ fi
 ################################################################################
 
 ## オプション > 保存先
-#if [[ ! -d "$HOME/Pictures/Screenshots" ]]; then
-#    mkdir -p "$HOME/Pictures/Screenshots"
-#fi
-#defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots"
+if [[ ! -d "$HOME/Pictures/Screenshots" ]]; then
+    mkdir -p "$HOME/Pictures/Screenshots"
+fi
+defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots"
 
 
 ################################################################################
@@ -234,7 +234,7 @@ fi
 ## 充電上限を80%にする
 ## PC名の「のmacbook」を消す
 ## Macノートブックの蓋を開けたり、電源に接続したときに電源が入らないようにする
-#sudo nvram BootPreference=%00
+sudo nvram BootPreference=%00
 
 
 ################################################################################
